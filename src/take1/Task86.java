d7c2845b2b771cec8e5712b08bac47a9dba67a5c
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-06-17 22:49:20 +0545

    Some updates

--- src/take1/Task86.java
+++ src/take1/Task86.java
@@ -1,8 +1,6 @@
 package take1;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Task86 {
     public static void main(String[] args) {
@@ -19,11 +17,49 @@ public class Task86 {
         vegetables.put(300, "Cauliflower");
         vegetables.put(100, "Cabbage");
         vegetables.put(50, "Lemon");
+        vegetables.put(550, "Lemons");
         vegetables.put(250, "Cucumber");
         Map<Integer, String> boughtvegetables = new TreeMap<>(vegetables);
         System.out.println(boughtvegetables);
 
+//      89.  Write a Java program to search a key in a Tree Map.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the key you want to search: ");
+        int key = sc.nextInt();
+        int count = 0;
+        for (int k : vegetables.keySet()) {
+            if (key == k) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            System.out.println("The key " + key + " does not exist.");
+        } else {
+            System.out.println("The value of the key " + key + " is: " + vegetables.get(key));
+        }
 
+//      90. Write a Java program to search a value in a Tree Map.
+        System.out.println("Enter the value you want to search: ");
+        String value = sc.next();
+        count = 0;
+        for (Map.Entry<Integer, String> entry : vegetables.entrySet()) {
+            if (entry.getValue().equalsIgnoreCase(value)) {
+                System.out.println("The key of the value " + value + " is: " + entry.getKey());
+                break;
+            }
+        }
+
+
+//      91.  Write a Java program to get all keys from a given Tree Map.
+        for (int k : treemap.keySet()) {
+            System.out.println(k + " = " + treemap.get(k));
+        }
+
+//      92.  Write a Java program to delete all elements from a given Tree Map.
+        treemap.clear();
+        System.out.println(treemap);
+
+//      93.  Write a Java program to sort keys in Treemap by using comparator
 
 
     }
