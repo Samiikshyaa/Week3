69d91cc96d06ef7cfa235e96050fd822a7d11ef2
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-06-18 07:39:07 +0545

    tree map using comparator

--- src/take1/Task93.java
+++ src/take1/Task93.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Task93 {
     //      93.  Write a Java program to sort keys in Treemap by using comparator
     public static void main(String[] args) {
-        Map<Integer, String> names = new LinkedHashMap<>((p1, p2) -> (Integer.compare(p1,p2)));
+        Map<Integer, String> names = new TreeMap<>((p1, p2) -> (Integer.compare(p1,p2)));
         names.put(2,"Luffy");
         names.put(1,"Zoro");
         names.put(4,"Nami");
